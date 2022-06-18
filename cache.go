f6d55223740e02f3fdb041c422bd419637f1035e
Zhainar Zhanekenov <go@email.com>
2022-06-18 20:57:10 +0600

    implemented simple cache

--- cache.go
+++ cache.go
@@ -2,22 +2,64 @@ package cache
 
 import "time"
 
+type cacheDeadline struct {
+	set  bool
+	till time.Time
+}
+
+func (d *cacheDeadline) expired() bool {
+	if !d.set {
+		return false
+	}
+
+	now := time.Now()
+
+	return now.Before(d.till)
+}
+
+type cacheItem struct {
+	value    string
+	deadline cacheDeadline
+}
+
 type Cache struct {
+	items map[string]cacheItem
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{items: make(map[string]cacheItem)}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (cache *Cache) Get(key string) (string, bool) {
+	item, ok := cache.items[key]
 
+	if !ok {
+		return "", false
+	}
+
+	if item.deadline.expired() {
+		return "", false
+	}
+
+	return item.value, true
 }
 
-func (receiver) Put(key, value string) {
+func (cache *Cache) Put(key, value string) {
+	cache.items[key] = cacheItem{value: value, deadline: cacheDeadline{}}
 }
 
-func (receiver) Keys() []string {
+func (cache *Cache) Keys() []string {
+	keys := make([]string, 0)
+
+	for k, item := range cache.items {
+		if !item.deadline.expired() {
+			keys = append(keys, k)
+		}
+	}
+
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cache *Cache) PutTill(key, value string, deadline time.Time) {
+	cache.items[key] = cacheItem{value: value, deadline: cacheDeadline{set: true, till: deadline}}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module juniormalanda/golang-united-school-homework-5.2
+
+go 1.16
